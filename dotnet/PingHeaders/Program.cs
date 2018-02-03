0e51b5683a675b71970f43fc62d694da19a24317
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-02-03 16:50:26 +0100

    Added dotnet visual studio example with Ping and PingHeaders

---
+++ dotnet/PingHeaders/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Xillio.ApiExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://sandbox.xill.io/v2/system/ping");
+            Console.WriteLine(response.Headers.ToString());
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+    }
+}
