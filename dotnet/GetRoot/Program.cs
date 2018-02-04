03aef1444c2b7ab3899580e0588906b4392845fd
Jeroen Huinink <jeroen@studio-h3o.nl>
2018-02-04 15:03:51 +0100

    Added GetRoot

---
+++ dotnet/GetRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xillio.ApiExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync(string[] args)
+        {
+            var clientid = args[0];
+            var clientsecret = args[1];
+            var username = args[2];
+            var password = args[3];
+
+            var token = await Utils.GetToken(clientid, clientsecret, username, password);
+            var result = await Utils.GetObject("https://sandbox.xill.io/v2/entities?scope=children", token);
+            Console.WriteLine(result.ToString());
+            foreach(var repo in result["children"].Children())
+            {
+                Console.WriteLine(repo["original"]["name"]["systemName"]);
+            }
+        }
+    }
+}
---
+++ dotnet/Utils/Utils.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Xillio.ApiExamples
+{
+    public static class Utils
+    {
+        public static async Task<JObject> GetObject(string url, string auth)
+        {
+            using (var client = new HttpClient())
+            {
+                var authValue = new AuthenticationHeaderValue("Bearer", auth);
+                client.DefaultRequestHeaders.Authorization = authValue;
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                var body = await response.Content.ReadAsStringAsync();
+                return JObject.Parse(body);
+            }
+        }
+
+        public static async Task<JArray> GetArray(string url, string auth)
+        {
+            using (var client = new HttpClient())
+            {
+                var authValue = new AuthenticationHeaderValue("Bearer", auth);
+                client.DefaultRequestHeaders.Authorization = authValue;
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                var body = await response.Content.ReadAsStringAsync();
+                
+                return JArray.Parse(body);
+            }
+        }
+
+        public static async Task<string> GetToken(string clientid, string clientsecret, string username, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                var authValue = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientid}:{clientsecret}")));
+                client.DefaultRequestHeaders.Authorization = authValue;
+                var parameters = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("grant_type", "password"),
+                    new KeyValuePair<string, string>("username", username),
+                    new KeyValuePair<string, string>("password", password),
+                };
+                var content = new FormUrlEncodedContent(parameters);
+
+                HttpResponseMessage response = await client.PostAsync("https://sandbox.xill.io/oauth/token", content);
+                var body = await response.Content.ReadAsStringAsync();
+                var o = JObject.Parse(body);
+                return (string)o["access_token"];
+            }
+        }
+    }
+}
